d265176f1840ff80a1e3fc9ee67ea52d753794ef
Niels Krijger <niels.krijger@icemobile.com>
2014-06-30 09:56:23 +0200

    Fixed reference to @new_resource.locations

--- resources/conf.rb
+++ resources/conf.rb
@@ -5,3 +5,4 @@ attribute :name, name_attribute: true, required: true
 attribute :port, kind_of: Integer, default: '8080', required: true
 attribute :server_name, kind_of: [String, Array], default: node['fqdn'], required: true
 attribute :path, kind_of: String, required: true
+attribute :locations, kind_of: Hash, required: true
